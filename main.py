26c2146208a2404bc26873616c074caf512b4ebb
Walker Logan <walker.logan44@gmail.com>
2024-01-08 18:59:10 -0600

    got basics done. UI design and downloadability. Needs testing

--- main.py
+++ main.py
@@ -9,5 +9,29 @@ app = customtkinter.CTk()
 app.geometry("500x500")
 app.title("YouTube Downloader")
 
+# UI things
+title = customtkinter.CTkLabel(app, text="Insert a YouTube link")
+title.pack(padx=12, pady=12)
+
+# link input
+url_var = tkinter.StringVar()
+link = customtkinter.CTkEntry(app, width=400, height=30, textvariable=url_var)
+link.pack()
+
+# download button
+def startDownload():
+    try:
+        ytLink = link.get()
+        ytObject = YouTube(ytLink)
+        ytVideo = ytObject.streams.get_highest_resolution()
+        ytVideo.download()
+    except:
+        print("Something is wrong with the YouTube link")
+    print("YouTube video downloaded successfully!")
+        
+        
+download = customtkinter.CTkButton(app, text="Download", command=startDownload)
+download.pack(padx=10, pady=10)
+
 # running the app
 app.mainloop()
