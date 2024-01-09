4abdabe259ccf08192f067240ccb0e07fe729c7a
Walker Logan <walker.logan44@gmail.com>
2024-01-08 19:45:06 -0600

    changed some window positioning and got successful video download

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 import tkinter
 import customtkinter
 from pytube import YouTube
+from pytube.exceptions import PytubeError
+from screeninfo import get_monitors
 
+# settings and customizations
 customtkinter.set_appearance_mode("system")
 customtkinter.set_default_color_theme("blue")
 
+# creating the app
+monitor = get_monitors()[0]
+center_x = monitor.width // 2
+center_y = monitor.height // 2
+window_width = 400
+window_height = 200
+position_x = center_x - window_width // 2
+position_y = center_y - window_height // 2
+
 app = customtkinter.CTk()
-app.geometry("500x500")
+app.geometry(f"{window_width}x{window_height}+{position_x}+{position_y}")
 app.title("YouTube Downloader")
 
 # UI things
@@ -16,7 +28,7 @@ title.pack(padx=12, pady=12)
 # link input
 url_var = tkinter.StringVar()
 link = customtkinter.CTkEntry(app, width=400, height=30, textvariable=url_var)
-link.pack()
+link.pack(padx=10, pady=10)
 
 # download button
 def startDownload():
@@ -25,9 +37,10 @@ def startDownload():
         ytObject = YouTube(ytLink)
         ytVideo = ytObject.streams.get_highest_resolution()
         ytVideo.download()
-    except:
-        print("Something is wrong with the YouTube link")
-    print("YouTube video downloaded successfully!")
+    except PytubeError as e:
+        print(f"An error occurred while downloading the video: {e}")
+    else:
+        print("YouTube video downloaded successfully!")
         
         
 download = customtkinter.CTkButton(app, text="Download", command=startDownload)
